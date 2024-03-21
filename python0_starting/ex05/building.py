daff06d8e5d4be3568987957639703433d8ab889
Maxil Cote <macote@c2r3p3.42quebec.com>
2024-03-21 16:50:06 -0400

    started ex05

---
+++ python0_starting/ex05/building.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+def get_input():
+    '''parses the arguments of the program'''
+
+    count = 0
+    for args in sys.argv:
+        for chars in args:
+            if chars.isalnum() or chars.isspace() or chars.is:
+                
+        
+        
+        
+
+
+def count_input():
+    '''Counts Alphanum and '''
+
+def main():
+    '''main'''
+    
+
+
+if __name__ == "__main__":
+    main()
